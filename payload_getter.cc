d049184c9a5db88badfc2e17b40a78fe60cc2386
Emanuel Dicu <emanueldicu2001@gmail.com>
2023-12-08 11:47:47 +0200

    Implement Homework 2 Part 3

---
+++ payload_getter.cc
@@ -0,0 +1,5 @@
+#include <string>
+
+std::string getPayload() {
+  return "Hello, World!";
+}
---
+++ test_app.cc
@@ -0,0 +1,7 @@
+#include "payload_getter.h"
+#include "payload_printer.h"
+
+int main() {
+  printPayload(getPayload());
+  return 0;
+}
