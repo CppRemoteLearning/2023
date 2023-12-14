7a37f61d2e267c244cccc46ba8a42971458e807d
Marius DUmitrescu <dumitrescumarius03@gmail.com>
2023-12-14 11:14:20 +0200

    add sensor class

---
+++ Sensor.cpp
@@ -0,0 +1,31 @@
+#include "Sensor.h"
+
+Sensor::Sensor(double temperature = 0, bool presence = false, double light=0,float humidity=0,char name[20]):nummberID(this->value++){
+    this->temperature = temperature;
+    this->presence = presence;
+    this->light =light;
+    this-> humidity = humidity;
+    strcpy(this->name,name);
+}
+int Sensor::value = 0;
+double Sensor::getTemperature()
+{
+    return this->temperature;
+}
+bool Sensor::getPresence()
+{
+    return this->presence;
+}
+double Sensor::getLight()
+{
+    return this->light;
+}
+float Sensor::getHumidity()
+{
+return this->humidity;
+}
+
+char* Sensor::getName()
+{
+    return this->name;
+}
---
+++ Sensor.h
@@ -0,0 +1,25 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+#include <cstring>
+
+class Sensor
+{
+private:
+double temperature;
+bool presence;
+double light;
+float humidity;
+char name[20];
+const int nummberID;
+static int value;
+public:
+Sensor(double temperature = 0, bool presence = false, double light=0,float humidity=0,char name[20]);
+double getTemperature();
+bool getPresence();
+double getLight();
+float getHumidity();
+char* getName();
+};
+
+#endif
