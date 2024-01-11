2b358a47e9c048aa1bf4e5e956b814d58d5a4ad1
AntoniaSzecsi <117035062+AntoniaSzecsi@users.noreply.github.com>
2024-01-11 19:46:59 +0200

    feat(unique-ptr): Added unique ptr implementation

--- main.cc
+++ main.cc
@@ -1,18 +1,33 @@
 #include <iostream>
 #include "user/User.h"
+#include "pointers/unique_ptr.h"
 
 int main() {
     srand(static_cast<unsigned>(time(nullptr)));
-    smart_home::User user(3);
-    user.getUserHouse().writeDataToFile();
+    smart_home::User& user = smart_home::User::getInstance();
+    smart_home::User::getUserHouse().writeDataToFile();
     std::cout << "Demo Control Devices\n";
     user.controlDevicesDemo();
     std::cout << std::endl;
     std::cout << "Demo Read Sensor Data From Room\n";
-    user.readSensorDataFromRoomDemo();
+    smart_home::User::readSensorDataFromRoomDemo();
     std::cout << std::endl;
     std::cout << "Demo Get Devices Status\n";
     user.getDevicesStatus();
+
+    std::cout << std::endl;
+    std::cout << "Demo Unique Pointer\n";
+    unique_ptr<int> ptr1(new int(10));
+    unique_ptr<int> ptr2(new int(20));
+    std::cout << *ptr1 << std::endl;
+    std::cout << *ptr2 << std::endl;
+    try {
+        ptr1 = std::move(ptr2);
+        std::cout << *ptr1 << std::endl;
+        std::cout << *ptr2 << std::endl;
+    } catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
 
     return 0;
 }
