282a758d6aae8e66e49b2454492b3ba05f191def
AntoniaSzecsi <117035062+AntoniaSzecsi@users.noreply.github.com>
2024-01-11 21:10:01 +0200

    feat(homework-6): my unique_ptr

--- main.cc
+++ main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "user/User.h"
+#include "pointers/unique_ptr.h"
 
 int main() {
     srand(static_cast<unsigned>(time(nullptr)));
@@ -13,6 +14,20 @@ int main() {
     std::cout << std::endl;
     std::cout << "Demo Get Devices Status\n";
     user.getDevicesStatus();
+
+    std::cout << std::endl;
+    std::cout << "Demo Unique Pointer\n";
+    unique_ptr<int> ptr1(new int(10));
+    unique_ptr<int> ptr2(new int(20));
+    std::cout << *ptr1 << std::endl;
+    std::cout << *ptr2 << std::endl;
+    try {
+        ptr1 = std::move(ptr2);
+        std::cout << *ptr1 << std::endl;
+        std::cout << *ptr2 << std::endl;
+    } catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
 
     return 0;
 }
