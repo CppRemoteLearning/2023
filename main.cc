c1ee111fe92b8990d94cbfaca99c8db7f11754f8
AntoniaSzecsi <117035062+AntoniaSzecsi@users.noreply.github.com>
2024-01-12 09:54:26 +0200

    fix(homework-6): fix PR comments

--- main.cc
+++ main.cc
@@ -1,24 +1,12 @@
 #include <iostream>
-#include "user/User.h"
 #include "pointers/unique_ptr.h"
 
 int main() {
     std::cout << "Demo Unique Pointer\n";
-    unique_ptr<int> singlePtr(new int(42));
+    my_unique_ptr::unique_ptr<int> singlePtr(new int(42));
     std::cout << "singlePtr points to: " << *singlePtr << std::endl;
 
     singlePtr.reset(new int(55));
     std::cout << "singlePtr now points to: " << *singlePtr << std::endl;
-
-    {
-        unique_ptr<int[]> arrayPtr(new int[3]{1, 2, 3});
-        std::cout << "arrayPtr points to an array: ";
-        for (int i = 0; i < 3; ++i) {
-            std::cout << arrayPtr[i] << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    std::cout << std::endl;
     return 0;
 }
--- pointers/unique_ptr.h
+++ pointers/unique_ptr.h
@@ -1,110 +1,66 @@
 #include <cstddef>
 #include <utility>
 
-template<typename T, typename Deleter = void(*)(T*)>
-class unique_ptr {
-private:
-    T* ptr;
-    Deleter deleter;
+namespace my_unique_ptr {
+    template<typename T, typename Deleter = void(*)(T*)>
+    class unique_ptr {
 
-    void deleter_call() {
-        if (deleter) {
-            deleter(ptr);
-        } else {
-            delete ptr;
-        }
-    }
+    public:
+        explicit unique_ptr(T* p = nullptr, Deleter d = [](T* p){ delete p; }) : ptr(p), deleter(d) {}
 
-public:
-    explicit unique_ptr(T* p = nullptr, Deleter d = [](T* p){ delete p; }) : ptr(p), deleter(d) {}
-
-    ~unique_ptr() {
-        deleter_call();
-    }
-
-    unique_ptr(unique_ptr&& u) noexcept : ptr(u.ptr), deleter(std::move(u.deleter)) {
-        u.ptr = nullptr;
-    }
-
-    unique_ptr& operator=(unique_ptr&& u) noexcept {
-        if (this != &u) {
+        ~unique_ptr() {
             deleter_call();
-            ptr = u.ptr;
-            deleter = std::move(u.deleter);
+        }
+
+        unique_ptr(unique_ptr&& u) noexcept : ptr(u.ptr), deleter(std::move(u.deleter)) {
             u.ptr = nullptr;
         }
-        return *this;
-    }
 
-    unique_ptr(const unique_ptr&) = delete;
-    unique_ptr& operator=(const unique_ptr&) = delete;
-
-    T& operator*() const { return *ptr; }
-    T* operator->() const { return ptr; }
-
-    T* release() {
-        T* temp = ptr;
-        ptr = nullptr;
-        return temp;
-    }
-
-    void reset(T* p = nullptr) {
-        deleter_call();
-        ptr = p;
-    }
-
-    T* get() const { return ptr; }
-    Deleter get_deleter() const { return deleter; }
-
-    T& operator[](std::size_t index) const {
-        return ptr[index];
-    }
-};
-
-template<typename T>
-class unique_ptr<T[]> {
-private:
-    T* ptr;
-
-public:
-    explicit unique_ptr(T* p = nullptr) : ptr(p) {}
-
-    ~unique_ptr() {
-        delete[] ptr;
-    }
-
-    unique_ptr(unique_ptr&& u) noexcept : ptr(u.ptr) {
-        u.ptr = nullptr;
-    }
-
-    unique_ptr& operator=(unique_ptr&& u) noexcept {
-        if (this != &u) {
-            delete[] ptr;
-            ptr = u.ptr;
-            u.ptr = nullptr;
+        unique_ptr& operator=(unique_ptr&& u) noexcept {
+            if (this != &u) {
+                deleter_call();
+                ptr = u.ptr;
+                deleter = std::move(u.deleter);
+                u.ptr = nullptr;
+            }
+            return *this;
         }
-        return *this;
-    }
 
-    unique_ptr(const unique_ptr&) = delete;
-    unique_ptr& operator=(const unique_ptr&) = delete;
+        unique_ptr(const unique_ptr&) = delete;
+        unique_ptr& operator=(const unique_ptr&) = delete;
 
-    T& operator[](std::size_t index) const {
-        return ptr[index];
-    }
+        T& operator*() const { return *ptr; }
+        T* operator->() const { return ptr; }
 
-    T* release() {
-        T* temp = ptr;
-        ptr = nullptr;
-        return temp;
-    }
-
-    void reset(T* p = nullptr) {
-        if (ptr) {
-            delete[] ptr;
+        T* release() {
+            T* temp = ptr;
+            ptr = nullptr;
+            return temp;
         }
-        ptr = p;
-    }
 
-    T* get() const { return ptr; }
-};
+        void reset(T* p = nullptr) {
+            deleter_call();
+            ptr = p;
+        }
+
+        T* get() const { return ptr; }
+        Deleter get_deleter() const { return deleter; }
+
+        T& operator[](std::size_t index) const {
+            return ptr[index];
+        }
+
+    private:
+        T* ptr;
+        Deleter deleter;
+
+        void deleter_call() {
+            if (deleter) {
+                deleter(ptr);
+            } else {
+                delete ptr;
+            }
+        }
+
+    };
+}
